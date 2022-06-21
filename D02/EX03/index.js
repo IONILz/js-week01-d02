0f54d96a8eb393d7d08e5467a759c7c790e5be30
Aviv <35579912+IONILz@users.noreply.github.com>
2022-06-21 23:06:36 +0300

    EX03

--- D02/EX03/index.js
+++ D02/EX03/index.js
@@ -2,7 +2,6 @@ function calculateMatrixColumnSum(matrix, columnIndex) {
   let sumOfColumn = 0;
   for (let i = 0; i < matrix.length; i++) {
     sumOfColumn = sumOfColumn + matrix[i][columnIndex];
-    console.log("Sum of Column " + columnIndex + ": " + sumOfColumn);
   }
   return sumOfColumn;
 };
